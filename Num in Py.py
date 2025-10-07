130efa5259622240f8a5f1e44fb6976f6db5f6c2
AstroLunaBot <156973806+AstroLunaBot@users.noreply.github.com>
2025-10-07 07:11:13 +0300

    Num in Py.py

---
+++ Num in Py.py
@@ -0,0 +1,18 @@
+Com = (10+20j)
+print(Com.imag)
+print(Com.real)
+
+N = 10
+print("{:.10f}".format(N))
+
+
+N1 = 159.605
+print(int(N1))
+print(type(int(N1)))
+
+
+print(100 - 115) #= -15
+print(50 * 30) #= 1500
+print(21 % 4) #= 1
+print(110 // 11) #= 10
+print(97 // 20) #= 4
