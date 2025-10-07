12fa3942e3f536098fc582f54277801b91e9899d
AstroLunaBot <156973806+AstroLunaBot@users.noreply.github.com>
2025-10-07 06:30:58 +0300

    My Code 0.py

--- My Code 0.py
+++ My Code 0.py
@@ -2,3 +2,12 @@ n = "Astro"
 a = 25
 c = "Russia"
 print("My name is {} and My age is {:d} and My Country is {}".format(n,a,c))
+
+Num = 10
+print("The Number is {:d}".format(Num))
+print("The Number is {:f}".format(Num))
+print("The Number is {:.2f}".format(Num))
+
+Hello = "Hello World i love Python"
+print("{:s}".format(Hello))
+print("{:.5s}".format(Hello))
