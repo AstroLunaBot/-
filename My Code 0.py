d3fe5c7292cdbd11b1e1891ab62089c5a6354379
AstroLunaBot <156973806+AstroLunaBot@users.noreply.github.com>
2025-10-07 06:45:50 +0300

    My Code 0.py

--- My Code 0.py
+++ My Code 0.py
@@ -11,3 +11,9 @@ print("The Number is {:.2f}".format(Num))
 Hello = "Hello World i love Python"
 print("{:s}".format(Hello))
 print("{:.5s}".format(Hello))
+
+a,b,c = "one", "two", "three"
+print("Hello {} {} {}".format(a,b,c))
+print("Hello {2} {1} {0}".format(a,b,c))
+
+print(f"My name is {n} and My age is {a} and My Country is {c}")
